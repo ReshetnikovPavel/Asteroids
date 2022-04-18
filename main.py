8d8dd69d05b513b4481edb24f11d040bb4c59275
AndreyKuklinov <Andreydolphin@mail.ru>
2022-04-18 11:16:33 +0500

    Сделал класс Game

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
 import pygame
+import pygame as pg
+
+
+class Game:
+    clock = pg.time.Clock()
+    run = True
+    game_over = False
+
+    @staticmethod
+    def run():
+        pg.display.set_caption('Asteroids')
+        while Game.run:
+            Game.clock.tick(60)
+            if not Game.game_over:
+                pass
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    Game.run = False
+        pg.quit()
+
+    @staticmethod
+    def draw():
+        pass
+
+
+# On start
+Game.run()
+
