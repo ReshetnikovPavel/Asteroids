35a33ce5450d911ef95313d3be17ae73f0c82e6c
Pavel Reshetnikov <92433826+ReshetnikovPavel@users.noreply.github.com>
2022-04-22 21:05:11 +0500

    Астероиды пересекают космическое пространство

--- asteroid.py
+++ asteroid.py
@@ -1,30 +1,29 @@
 from Assets import Textures
 import random
-import main
 import pygame.math as m
 
 
 class Asteroid:
-    def __init__(self, rank):  # rank - размер астероида
+    def __init__(self, rank, screen_width, screen_height):  # rank - размер астероида
         self.rank = rank
-        self.texture = self._set_texture(rank)
+        self.texture = self._rank_to_texture(rank)
         self.width = 50 * rank
         self.height = 50 * rank
         self.spawn_point = random.choice(
-            [(random.randrange(0, main.screen_width - self.width),
-              random.choice([-1 * self.height - 5, main.screen_height + 5])),
-             (random.choice([-1 * self.width - 5, main.screen_width + 5]),
-              random.randrange(0, main.screen_height - self.height))])
+            [(random.randrange(0, screen_width - self.width),
+              random.choice([-1 * self.height - 5, screen_height + 5])),
+             (random.choice([-1 * self.width - 5, screen_width + 5]),
+              random.randrange(0, screen_height - self.height))])
 
         self.x, self.y = self.spawn_point
-        self.direction = m.Vector2(1 if self.x < main.screen_width//2 else -1,
-                                   1 if self.y < main.screen_height//2 else -1)
+        self.direction = m.Vector2(1 if self.x < screen_width//2 else -1,
+                                   1 if self.y < screen_height//2 else -1)
 
         self.velocity = m.Vector2(self.direction.x * random.randrange(1, 3),
                                   self.direction.y * random.randrange(1, 3))
 
     @staticmethod
-    def _set_texture(rank):
+    def _rank_to_texture(rank):
         textures = Textures()
         random_number = random.randint(0, 3)
         if rank == 1:
--- main.py
+++ main.py
@@ -1,21 +1,36 @@
 import pygame as pg
+import random
+
+from asteroid import Asteroid
 
 screen_width = 800
 screen_height = 800
 
+
+
 class Game:
     clock = pg.time.Clock()
-    run = True
+    asteroids = []
+    count = 0
+    is_run = True
     game_over = False
     screen = pg.display.set_mode((screen_width, screen_height))
+
+
 
     @staticmethod
     def run():
         pg.display.set_caption('Asteroids')
-        while Game.run:
+        while Game.is_run:
             Game.clock.tick(60)
+            Game.count += 1
             if not Game.game_over:
+                if Game.count % 50 == 0:
+                    random_number = random.choice([1, 1, 1, 2, 2, 3])
+                    Game.asteroids.append(Asteroid(random_number, screen_width, screen_height))
                 Game.draw()
+
+                Game.move_asteroids()
             for event in pg.event.get():
                 if event.type == pg.QUIT:
                     Game.run = False
@@ -24,10 +39,18 @@ class Game:
     @staticmethod
     def draw():
         Game.screen.fill((0, 0, 0))
-
+        for asteroid in Game.asteroids:
+            asteroid.draw(Game.screen)
         pg.display.update()
+
+    @staticmethod
+    def move_asteroids():
+        for asteroid in Game.asteroids:
+            asteroid.x += asteroid.velocity.x
+            asteroid.y += asteroid.velocity.y
 
 
 # On start
 Game.run()
+
 
